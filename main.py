2087b7ac28442dcdad06ae99f197006fb448fad8
J91-cloud <jessygjerek2@gmail.com>
2025-04-04 08:59:40 -0400

    Exporting files

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+
+# Configure templates directory
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI(title="Resume CMS API")
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to your Resume CMS!"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(
+        "main/index.html",  
+        {"request": request}  
+    )
